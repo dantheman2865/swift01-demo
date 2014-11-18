fff7c14a537b575043f920fc851011e8a58e69f7
Daniel Kurin <dkurin@swiftlet.technology>
2014-11-17 20:02:07 -0500

    Revert "Upload of 2014-11-16 Demo"
    This reverts commit 3fb4229407b791d6a04a64f7a57ea93889094572.

--- COORD_NCP1/COORD_NCP1/Debug/src/ASF/thirdparty/wireless/avr2025_mac/source/mac/src/mac_security.d
+++
@@ -1,3 +0,0 @@
-src/ASF/thirdparty/wireless/avr2025_mac/source/mac/src/mac_security.d \
- src/ASF/thirdparty/wireless/avr2025_mac/source/mac/src/mac_security.o: \
- ../src/ASF/thirdparty/wireless/avr2025_mac/source/mac/src/mac_security.c
--- DEV_NCP1/DEV_NCP1/Debug/src/ASF/thirdparty/wireless/avr2025_mac/source/stb/src/stb_armcrypto.d
+++
@@ -1,3 +0,0 @@
-src/ASF/thirdparty/wireless/avr2025_mac/source/stb/src/stb_armcrypto.d \
- src/ASF/thirdparty/wireless/avr2025_mac/source/stb/src/stb_armcrypto.o: \
- ../src/ASF/thirdparty/wireless/avr2025_mac/source/stb/src/stb_armcrypto.c
